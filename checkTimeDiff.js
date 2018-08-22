d3efd9ac50ba120f53af162f5aed64811fd0649e
ksard <kolyo.sardamov@gmail.com>
2018-08-23 02:43:26 +0300

    with moment

--- checkTimeDiff.js
+++ checkTimeDiff.js
@@ -1,28 +1,36 @@
-// var child_process = require('child_process');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var moment = require('moment');
-//below is the local server time
-var localServerTime = new Date(Date.now()).toTimeString();
+var request = require('request');
 
-//below is remote server time
-var xmlHttp;
-function srvTime(){
-    try {
-        //FF, Opera, Safari, Chrome
-        xmlHttp = new XMLHttpRequest();
-    }
-    catch (err) {
-      console.log(err);
-    }
-    xmlHttp.open('HEAD','https://www.google.com'.toString(),false);
-    xmlHttp.setRequestHeader("Content-Type", "text/html");
-    xmlHttp.send('');
-    return xmlHttp.getResponseHeader("Date");
-}
 
-var st = srvTime();
-var rem = new Date(st).toTimeString();
-console.log(st);
-//for less than 24 hours difference use below:
-var timeDiff = moment.utc(moment(localServerTime,"HH:mm:ss.SSSZ").diff(moment(localServerTime,"HH:mm:ss.SSSZ"))).format("HH:mm:ss.SSSZ")
-console.log(timeDiff);
+// Below won’t give the actual time that request takes. 
+// Below request call is async, and we start measuring time at the time 
+// when request was queued, not actually sent.
+// let start_time = new Date().getTime();
+
+// request.get('https://google.com', function (err, response) {
+//     console.log('Time elapsed since queuing the request:', new Date().getTime() - start_time);
+// });
+
+//thus I am using the time parameter
+request.get({ url: 'http://www.google.com', time: true }, function (err, response) {
+    var remoteServerTime = response.headers.date;
+    var requestTime = response.elapsedTime;
+    calculateTimeDiff(remoteServerTime, requestTime);
+    //console.log('the request headers date ' + response.headers.date);
+    //console.log('The actual time elapsed:', response.elapsedTime);
+    //console.log(response.timings);
+});
+
+function calculateTimeDiff(remoteServerTime, requestTime){
+    var localServerTime = new Date(Date.now()).toTimeString();
+    var formattedLocal = moment(localServerTime, "HH:mm:ss").format("hh:mm:ss A");
+    var formattedRemote = moment(remoteServerTime, "HH:mm:ss").format("hh:mm:ss A");
+    //console.log(formattedLocal)
+    var timeDiff = moment.utc(moment(formattedLocal,"HH:mm:ss.SSSZ").diff(moment(formattedRemote,"HH:mm:ss.SSSZ"))).format("HH:mm:ss.SSSZ")
+    
+    var ms = moment(formattedLocal,"DD/MM/YYYY HH:mm:ss").diff(moment(formattedRemote,"DD/MM/YYYY HH:mm:ss"));
+    var d = moment.duration(ms);
+    var s = d.format("hh:mm:ss");
+    console.log(s);
+};
