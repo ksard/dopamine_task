1c05868b958590548591ed082400b20f9d50bf31
ksard <kolyo.sardamov@gmail.com>
2018-08-24 00:51:21 +0300

    fixces

--- checkTimeDiff.js
+++ checkTimeDiff.js
@@ -1,7 +1,6 @@
 var moment = require('moment');
 var request = require('request');
 
-//thus I am using the time parameter
 request.get({ url: 'http://localhost:3050/getTime' }, function (err, response) {
     if(response.body){
         var resJson = JSON.parse(response.body);
@@ -12,7 +11,9 @@ request.get({ url: 'http://localhost:3050/getTime' }, function (err, response) {
 });
 
 function calculateTimeDiff(remoteServerTime, networkDelay){
+    //the network delay should be added to the remote server time since the current time 
+    //is now and the remote server time is the delay plus the remote server time
     var localServerTime = new Date(Date.now()).toLocaleString();
-    var timeDiff = moment.utc(moment(localServerTime,"MM/DD/YYYY HH:mm:ss.SSSZ").diff(moment(remoteServerTime,"MM/DD/YYYY HH:mm:ss.SSSZ"))).format("HH:mm:ss.SSSZ")
+    var timeDiff = moment.utc(moment(localServerTime,"MM/DD/YYYY HH:mm:ss.SSSZ").diff(moment(remoteServerTime,"MM/DD/YYYY HH:mm:ss.SSSZ"))).format("HH:mm:ss."+networkDelay+"")
     console.log(timeDiff)
 };
